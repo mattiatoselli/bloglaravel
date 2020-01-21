6a9848c6764f14dab3c402875b0180c7d4b7e620
mattiatoselli <44556380+mattiatoselli@users.noreply.github.com>
2020-01-21 16:16:06 +0100

    Update child.blade.php

--- resources/views/child.blade.php
+++ resources/views/child.blade.php
@@ -10,6 +10,16 @@
     <p>This is appended to the master sidebar.</p>
 @endsection
 
+@section('summary')
+    <p>This is my body ssss.</p><br>
+    <p>This is my body sssss.</p><br>
+    <p>This is my body conssssstent.</p><br>
+    <p>This is my body ss.</p><br>
+    <p>This is my body contentssss.</p><br>
+    <p>This is my body content.</p><br>
+@endsection
+
+
 @section('content')
     <p>This is my body content.</p><br>
     <p>This is my body content.</p><br>
