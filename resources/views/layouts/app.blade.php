4dfe71fd424e458364960c1aa979e0bac5bd340a
mattiatoselli <44556380+mattiatoselli@users.noreply.github.com>
2020-01-21 12:50:07 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,9 @@
 <html>
     <head>
         <title>App Name - @yield('title')</title>
+        <link href="assets/startbootstrap-freelancer/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
         <link rel="stylesheet" type="text/css" href="{{ asset('assets/startbootstrap-freelancer/css/freelancer.css') }}">
     </head>
     <body>
