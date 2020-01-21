62800510f7e9116a2ec0a751a9166dfeafac2203
mattiatoselli <44556380+mattiatoselli@users.noreply.github.com>
2020-01-21 16:07:53 +0100

    Update child.blade.php

--- resources/views/child.blade.php
+++ resources/views/child.blade.php
@@ -1,13 +1,21 @@
+<!-- Stored in resources/views/child.blade.php -->
+
+@extends('layouts.app')
+
 @section('title', 'Page Title')
 
 @section('navbar')
     @parent
+
     <p>This is appended to the master sidebar.</p>
-    <p class="mb-5">This is my body content.</p><br>
-    <a href="#primo">vai al primo paragrafo</a>
+@endsection
+
+@section('content')
+    <p>This is my body content.</p>
 @endsection
 
 @section('footer')
-    @parent
-    <p>This is appended to the master footer.</p>
+@parent
+
+    <p>This is appended to the master sidebar.</p>
 @endsection
