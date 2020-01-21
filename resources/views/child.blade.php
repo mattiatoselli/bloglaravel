72c8d174ef1018d83a5a126763d6afd53d88b315
mattiatoselli <44556380+mattiatoselli@users.noreply.github.com>
2020-01-21 16:46:24 +0100

    Update child.blade.php

--- resources/views/child.blade.php
+++ resources/views/child.blade.php
@@ -9,7 +9,7 @@
 @endsection
 
 @section('summary')
-    <p>This is my body ssss.</p><br>
+    <a href="#1">This is my body ssss.</a><br>
     <p>This is my body sssss.</p><br>
     <p>This is my body conssssstent.</p><br>
     <p>This is my body ss.</p><br>
@@ -17,14 +17,20 @@
     <p>This is my body content.</p><br>
 @endsection
 
-
 @section('content')
-    <p>This is my body content.</p><br>
-    <p>This is my body content.</p><br>
-    <p>This is my body content.</p><br>
-    <p>This is my body content.</p><br>
-    <p>This is my body content.</p><br>
-    <p>This is my body content.</p><br>
+    <hr id="1">
+    <strong>
+    strong
+    </strong>
+    </hr>
+    <p class="mb-5">
+    <hr>
+    <strong>
+    strong
+    </strong>
+    </hr>
+    Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque pretium dapibus quam cursus venenatis. Praesent hendrerit molestie enim, at dignissim metus egestas eu. Proin massa sapien, laoreet nec congue vel, feugiat eu tellus. Etiam interdum leo id massa imperdiet tempus. Nullam malesuada semper congue. Etiam luctus nisl ac porta malesuada. Phasellus sollicitudin ligula vitae diam consequat, et semper nisl ultricies. Nullam tincidunt maximus elit, ut bibendum elit eleifend volutpat. Quisque ultrices, leo sit amet volutpat pretium, arcu diam vehicula tellus, ac iaculis urna lectus quis quam. Proin cursus magna id tincidunt faucibus. Sed sodales facilisis tristique. 
+    </p>
 @endsection
 
 @section('footer')
