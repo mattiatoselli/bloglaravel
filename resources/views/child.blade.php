772a553f051ae37c202c1dcb3a20cb04c543ee71
mattiatoselli <44556380+mattiatoselli@users.noreply.github.com>
2020-01-21 15:56:41 +0100

    Update child.blade.php

--- resources/views/child.blade.php
+++ resources/views/child.blade.php
@@ -7,12 +7,6 @@
 @section('navbar')
 @endsection
 
-@section('summary')
-    <li class="nav-item mx-0 mx-lg-1">
-        <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">Collabora</a>
-    </li>
-@endsection
-
 @section('content')
     <p class="mb-5">This is my body content.</p><br>
     <a href="#primo">vai al primo paragrafo</a>
