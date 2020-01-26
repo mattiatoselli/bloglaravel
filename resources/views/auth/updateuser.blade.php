27bd6d1d67a6aa964ab30c7d556bae8e8bc1d081
mattiatoselli <44556380+mattiatoselli@users.noreply.github.com>
2020-01-26 12:46:04 +0100

    Update updateuser.blade.php

--- resources/views/auth/updateuser.blade.php
+++ resources/views/auth/updateuser.blade.php
@@ -53,6 +53,21 @@
                                 @enderror
                             </div>
                         </div>
+                        
+                        <div class="form-group row">
+                            <label for="description" class="col-md-4 col-form-label text-md-right">Descrizione</label>
+
+                            <div class="col-md-6">
+                            <textarea id="description" value="{{ $user->description }}" class="form-control @error('description') is-invalid @enderror" name="description" value="{{ old('description') }}"class="form-control" rows="3"></textarea>
+                                
+
+                                @error('description')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
