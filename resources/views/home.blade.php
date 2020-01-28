70cc6344eb2d3cef397f1b19f9019bba63a2a66c
mattiatoselli <44556380+mattiatoselli@users.noreply.github.com>
2020-01-28 10:43:32 +0100

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -54,4 +54,55 @@
             </div>
         </div>
     </div>
+    
+    
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Crea un nuovo argomento</div>
+
+                <div class="card-body">
+                    <form method="POST" action="argument">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="title" class="col-md-4 col-form-label text-md-right">Titolo</label>
+
+                            <div class="col-md-6">
+                                <input id="title" value="" type="text" class="form-control @error('title') is-invalid @enderror" name="title" value="{{ old('title') }}" required autofocus>
+
+                                @error('title')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        
+                        <div class="form-group row">
+                            <label for="image" class="col-md-4 col-form-label text-md-right">Titolo</label>
+
+                            <div class="col-md-6">
+                                <input id="image" value="" type="text" class="form-control @error('image') is-invalid @enderror" name="image" value="{{ old('image') }}" required autofocus>
+
+                                @error('image')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Modifica
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
