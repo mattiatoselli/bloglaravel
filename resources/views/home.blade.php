10972d3d72d9e8d0ab1682971a56d753b3acb880
mattiatoselli <44556380+mattiatoselli@users.noreply.github.com>
2020-01-28 10:44:22 +0100

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -80,7 +80,7 @@
                         </div>
                         
                         <div class="form-group row">
-                            <label for="image" class="col-md-4 col-form-label text-md-right">Titolo</label>
+                            <label for="image" class="col-md-4 col-form-label text-md-right">Url Immagine</label>
 
                             <div class="col-md-6">
                                 <input id="image" value="" type="text" class="form-control @error('image') is-invalid @enderror" name="image" value="{{ old('image') }}" required autofocus>
