2f7390160eaf106d9e28e2fe39951ba479383b94
mattiatoselli <44556380+mattiatoselli@users.noreply.github.com>
2020-01-28 11:02:49 +0100

    Update ArgumentsController.php

--- app/Http/Controllers/ArgumentsController.php
+++ app/Http/Controllers/ArgumentsController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Argument;
 
 class ArgumentsController extends Controller
 {
-    //
+    public function index($id)
+    {
+        $argument = Argument::find($id);
+        return view('summary', ['users' => $staff, 'argument' => $argument]);
+    }
 }
