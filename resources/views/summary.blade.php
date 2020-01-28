de544e8e7f2aa6ef3eda29925423f1acb180a217
mattiatoselli <44556380+mattiatoselli@users.noreply.github.com>
2020-01-28 11:05:30 +0100

    Create summary.blade.php

---
+++ resources/views/summary.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.customapp')
+
+@section('title', 'Page Title')
+
+@section('navbar')
+@parent
+@endsection
+
+
+
+
+@section('content')
+@parent
+sezione sommario dell'argomento--------
+@endsection
+
+@section('about')
+@parent
+@endsection
+
+@section('footer')
+@parent
+@endsection
