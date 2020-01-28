5ca82c5f7e5ca12990a16a608247dea18c533935
mattiatoselli <44556380+mattiatoselli@users.noreply.github.com>
2020-01-28 11:07:28 +0100

    Update ArgumentsController.php

--- app/Http/Controllers/ArgumentsController.php
+++ app/Http/Controllers/ArgumentsController.php
@@ -9,6 +9,7 @@ class ArgumentsController extends Controller
 {
     public function index($id)
     {
+        $staff = User::where('staffrole', '1')->get();
         $argument = Argument::find($id);
         return view('summary', ['users' => $staff, 'argument' => $argument]);
     }
