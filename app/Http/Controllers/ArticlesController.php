f08a4a7a8d982016581074c5eb3a05fba001d1f8
mattiatoselli <44556380+mattiatoselli@users.noreply.github.com>
2020-01-28 17:33:26 +0100

    Update ArticlesController.php

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Argument:
+use App\Article;
 class ArticlesController extends Controller
 {
-    //
+    public function store()
+    {
+        dd('eccomi');
+    }
 }
