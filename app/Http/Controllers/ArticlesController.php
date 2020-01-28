29afa6d2ad759f49523a65053980e9ca9419169f
mattiatoselli <44556380+mattiatoselli@users.noreply.github.com>
2020-01-28 17:59:38 +0100

    Update ArticlesController.php

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -3,13 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Argument;
 use App\Article;
 class ArticlesController extends Controller
 {
     public function store()
     {
-        $argument = Argument::find(request('argument'));
-        dd(request('argument'));
+        $user = Auth::user();
+        $argument = Argument::where('title',request('argument'))->get();
+        $argId = $argument[0]->id;
+        $validatedData = request()->validate([
+        'title' => ['required', 'string', 'max:80', 'min:12'],
+        'body' => ['required', 'string', 'min:255'],
+        ]);
+        $article = new Article();
+        $article->title = request('title');
+        $article->body = request('body');
+        dd($user->id);
     }
 }
