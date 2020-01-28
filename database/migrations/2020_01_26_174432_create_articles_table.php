a8e86f867afd11347e6cf8ac95a7e8a61985c1af
mattiatoselli <44556380+mattiatoselli@users.noreply.github.com>
2020-01-28 15:03:11 +0100

    Update 2020_01_26_174432_create_articles_table.php

--- database/migrations/2020_01_26_174432_create_articles_table.php
+++ database/migrations/2020_01_26_174432_create_articles_table.php
@@ -14,6 +14,7 @@ class CreateArticlesTable extends Migration
     public function up()
     {
         Schema::create('articles', function (Blueprint $table) {
+            
             $table->bigIncrements('id');
             $table->bigInteger('userId'); //User Id
             $table->string('title'); //titolo dell'articolo
