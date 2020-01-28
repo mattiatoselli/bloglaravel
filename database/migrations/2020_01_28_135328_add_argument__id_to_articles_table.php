5ce1849da20244eca6ec6905a698b6b6c4df16b1
mattiatoselli <44556380+mattiatoselli@users.noreply.github.com>
2020-01-28 14:57:12 +0100

    Update 2020_01_28_135328_add_argument__id_to_articles_table.php

--- database/migrations/2020_01_28_135328_add_argument__id_to_articles_table.php
+++ database/migrations/2020_01_28_135328_add_argument__id_to_articles_table.php
@@ -14,7 +14,7 @@ class AddArgumentIdToArticlesTable extends Migration
     public function up()
     {
         Schema::table('articles', function (Blueprint $table) {
-            //
+            $table->bigInteger("argumentId");
         });
     }
 
@@ -26,7 +26,7 @@ class AddArgumentIdToArticlesTable extends Migration
     public function down()
     {
         Schema::table('articles', function (Blueprint $table) {
-            //
+            $table->dropColumn("argumentId");
         });
     }
 }
