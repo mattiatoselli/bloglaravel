262b3a100e5140e7c6fcfbdd1294ce3f8c650dd8
mattiatoselli <44556380+mattiatoselli@users.noreply.github.com>
2020-01-28 16:28:44 +0100

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Argument
 
 class HomeController extends Controller
 {
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $arguments = Argument::all();
+        return view('home', ['arguments' => $arguments]);
     }
 }
