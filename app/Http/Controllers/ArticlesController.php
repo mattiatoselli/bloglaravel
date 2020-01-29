0e212c249dc4785b3545e4bac62ae4c06cfa3ae2
mattiatoselli <44556380+mattiatoselli@users.noreply.github.com>
2020-01-29 10:53:07 +0100

    Update ArticlesController.php

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Auth;
 use App\Argument;
 use App\Article;
 class ArticlesController extends Controller
