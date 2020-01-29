4b1951973c1408f0a6f059c40a9abc1e12c86916
mattiatoselli <44556380+mattiatoselli@users.noreply.github.com>
2020-01-29 10:53:46 +0100

    Update ArticlesController.php

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+//use Illuminate\Support\Facades\Auth;
 use Auth;
 use App\Argument;
 use App\Article;
