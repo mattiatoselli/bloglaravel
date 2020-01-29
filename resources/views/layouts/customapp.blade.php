a7287681fb8e89a49e8d0cd85e8ffbe563cf003d
mattiatoselli <44556380+mattiatoselli@users.noreply.github.com>
2020-01-29 12:16:48 +0100

    Update customapp.blade.php

--- resources/views/layouts/customapp.blade.php
+++ resources/views/layouts/customapp.blade.php
@@ -36,7 +36,7 @@
     <div class="collapse navbar-collapse" id="navbarResponsive">
       <ul class="navbar-nav ml-auto">
       <li class="nav-item mx-0 mx-lg-1">
-        <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#chi-siamo">Chi siamo</a>
+        <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="{{ route('index') }}#chi-siamo">Chi siamo</a>
       </li>
       <li class="nav-item mx-0 mx-lg-1">
         <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">Collabora</a>
