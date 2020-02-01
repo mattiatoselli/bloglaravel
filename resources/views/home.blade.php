e8ff7ee0e903803c87a8d3f208a49915903db16b
mattiatoselli <44556380+mattiatoselli@users.noreply.github.com>
2020-02-01 02:12:41 +0100

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@
                 </div>
 
                 <div class="card-body">
-                    <form method="POST" action="article">
+                    <form method="GET" action="article">
                         @csrf
                         <div class="form-group row">
                             <label for="argument" class="col-md-4 col-form-label text-md-right">Argomento</label>
