2021edc7806ca8c18537c2ef211927076493970d
mattiatoselli <44556380+mattiatoselli@users.noreply.github.com>
2020-02-06 12:08:50 +0100

    Update app.php

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://localhost'),
+    'url' => env('APP_URL', 'https://www.kernelninjas.net'),
 
     'asset_url' => env('ASSET_URL', null),
 
